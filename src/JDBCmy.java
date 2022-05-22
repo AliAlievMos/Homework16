b28cd6e8e1fb443a95bd70426bb68eb6c3afb410
Ali Aliev <ali.aliev.msk>
2022-05-22 14:50:20 +0300

    17lesson

--- src/JDBCmy.java
+++ src/JDBCmy.java
@@ -8,6 +8,8 @@ public class JDBCmy {
     static final String JDBC_DRIVER = "org.sqlite.JDBC";
     static final String DATABASE_URL = "jdbc:sqlite:my.db";
     public static String table;
+    public static String action;
+    public static String id;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Scanner sc = new Scanner(System.in);
@@ -29,12 +31,43 @@ public class JDBCmy {
         statement = connection.createStatement();
 
 
-        showTable(statement);
+        menu(statement);
+
+
+
 
 
         System.out.println("Closing connection and releasing resources...");
         statement.close();
         connection.close();
+        sc.close();
+    }
+    public static void menu(Statement statement) throws SQLException {
+        System.out.println("1. Вывети таблицу\n2. Удалить эллемент");
+        Scanner sc = new Scanner(System.in);
+        action = sc.next();
+        if (Objects.equals(action, "1")){
+            showTable(statement);
+        }
+        if (Objects.equals(action, "2")){
+            deleteItem(statement);
+        }
+    }
+    public static void deleteItem(Statement st) throws SQLException {
+
+        String sql;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Что удалить?");
+        id = sc.next();
+
+        sql = "DELETE FROM " + table + " WHERE id=" + "'"+id+"'";
+
+        Statement statement = st;
+        ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.deleteRow();
+
+        st.close();
+
     }
 
     public static void showTable(Statement st) throws SQLException {
@@ -51,23 +84,27 @@ public class JDBCmy {
                 while (resultSet.next()) {
                     String name = resultSet.getString("name");
                     int adress = resultSet.getInt("adress");
+                    int id = resultSet.getInt("id");
                     System.out.println("Name: " + name);
                     System.out.println("Adress: " + adress);
+                    System.out.println("ID: " + id);
                     System.out.println("\n================\n");
                 }
             }
 //            name TEXT, color TEXT, model TEXT, speed CHAR
-            if (Objects.equals(table, "cars")) {
-                    while (resultSet.next()) {
-                        String name = resultSet.getString("name");
-                        String color = resultSet.getString("color");
-                        String model = resultSet.getString("model");
-                        int speed = resultSet.getInt("speed");
-                        System.out.println("Name: " + name);
-                        System.out.println("Сolor: " + color);
-                        System.out.println("Model: " + model);
-                        System.out.println("Speed: " + speed);
-                        System.out.println("\n================\n");
+        if (Objects.equals(table, "cars")) {
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String color = resultSet.getString("color");
+                String model = resultSet.getString("model");
+                int speed = resultSet.getInt("speed");
+                int id = resultSet.getInt("id");
+                System.out.println("Name: " + name);
+                System.out.println("Сolor: " + color);
+                System.out.println("Model: " + model);
+                System.out.println("Speed: " + speed);
+                System.out.println("ID: " + id);
+                System.out.println("\n================\n");
                     }
                 }
             if (Objects.equals(table, "users")) {
@@ -76,10 +113,12 @@ public class JDBCmy {
                         String surname = resultSet.getString("surname");
                         int money = resultSet.getInt("money");
                         int birthday = resultSet.getInt("birthday");
+                        int id = resultSet.getInt("id");
                         System.out.println("Name: " + name);
                         System.out.println("Surname: " + surname);
                         System.out.println("Money: " + money);
                         System.out.println("Birthday: " + birthday);
+                        System.out.println("ID: " + id);
                         System.out.println("\n================\n");
                     }
                 }
